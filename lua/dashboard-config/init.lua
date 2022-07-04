2a64afd92e46cd792c5f30e610ddf892dfafa6f0
Elia Cunial <elia.cunial@nhoa.energy>
2022-07-04 10:13:24 +0200

    Better dir management

--- lua/dashboard-config/init.lua
+++ lua/dashboard-config/init.lua
@@ -1,25 +1,17 @@
 local db = require('dashboard')
 local home = os.getenv('HOME')
 db.preview_command = 'cat | lolcat -F 0.3'
-db.preview_file_path = home .. '/.config/nvim/static/neovim.cat'
+db.preview_file_path = home .. '/.config/nvim/lua/dashboard-config/neovim.cat'
 db.preview_file_height = 16
 db.preview_file_width = 26
 
--- vim.g.dashboard_default_executive = 'telescope'
-
 db.custom_center = {
-  { icon = '   ', desc = 'Find File      ', shortcut = 'SPC f f', action = 'Telescope find_files' },
-  { icon = '   ', desc = 'Search Text    ', shortcut = 'SPC f r', action = 'Telescope live_grep' },
-  { icon = '   ', desc = 'Recent Files   ', shortcut = 'SPC f o', action = 'Telescope oldfiles' },
-  {
-    icon = '   ',
-    desc = 'Config           ',
-    shortcut = 'SPC c',
-    action = 'edit ~/.config/nvim/init.lua'
-  }, {
+  { icon = '   ', desc = 'Find File   ', action = 'Telescope find_files' },
+  { icon = '   ', desc = 'Search Text ', action = 'Telescope live_grep' },
+  { icon = '   ', desc = 'Recent Files', action = 'Telescope oldfiles' },
+  { icon = '   ', desc = 'Config      ', action = 'edit ~/.config/nvim/init.lua' }, {
     icon = '   ',
-    desc = 'Git            ',
-    shortcut = 'SPC u l',
+    desc = 'Git         ',
     action = 'lua require("toggleterm.terminal").Terminal:new({cmd="lazygit", direction = "float"}):toggle()'
   }
 }
