a48890c4248d6ff1142ed84b13cfef4d5d73615d
Elia Cunial <elia.cunial@nhoa.energy>
2022-07-04 10:41:52 +0200

    Dashboard sessions

--- lua/dashboard-config/init.lua
+++ lua/dashboard-config/init.lua
@@ -4,14 +4,16 @@ db.preview_command = 'cat | lolcat -F 0.3'
 db.preview_file_path = home .. '/.config/nvim/lua/dashboard-config/neovim.cat'
 db.preview_file_height = 16
 db.preview_file_width = 26
+db.session_directory = home .. '/.local/share/neovim_sessions'
 
 db.custom_center = {
-  { icon = '   ', desc = 'Find File   ', action = 'Telescope find_files' },
-  { icon = '   ', desc = 'Search Text ', action = 'Telescope live_grep' },
-  { icon = '   ', desc = 'Recent Files', action = 'Telescope oldfiles' },
-  { icon = '   ', desc = 'Config      ', action = 'edit ~/.config/nvim/init.lua' }, {
+  { icon = '   ', desc = 'Latest session', action = 'SessionLoad' },
+  { icon = '   ', desc = 'Find File     ', action = 'Telescope find_files' },
+  { icon = '   ', desc = 'Search Text   ', action = 'Telescope live_grep' },
+  { icon = '   ', desc = 'Recent Files  ', action = 'Telescope oldfiles' },
+  { icon = '   ', desc = 'Config        ', action = 'edit ~/.config/nvim/init.lua' }, {
     icon = '   ',
-    desc = 'Git         ',
+    desc = 'Git           ',
     action = 'lua require("toggleterm.terminal").Terminal:new({cmd="lazygit", direction = "float"}):toggle()'
   }
 }
