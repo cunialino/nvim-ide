e5176db8bc1b86fea6f7113144fe3dbec91ad6c2
Elia Cunial <elia.cunial@nhoa.energy>
2022-07-07 14:35:05 +0200

    Commented unuseful stuff

--- lua/ide-settings/init.lua
+++ lua/ide-settings/init.lua
@@ -1,20 +1,22 @@
-local util = vim.lsp.util
-local log = require 'vim.lsp.log';
-local function location_handler(_, method, result)
-  local _ = log.info() and log.info("ciao")
-  if result == nil or vim.tbl_isempty(result) then
-    local _ = log.info() and log.info(method, 'No location found')
-    return nil
-  end
-
-  if vim.tbl_islist(result) then
-    util.jump_to_location(result[1])
-
-    if #result > 1 then util.set_qflist(util.locations_to_items(result)) end
-  else
-    util.jump_to_location(result)
-  end
-end
+--local util = vim.lsp.util
+--local log = require("vim.lsp.log")
+--local function location_handler(_, method, result)
+--	local _ = log.info() and log.info("ciao")
+--	if result == nil or vim.tbl_isempty(result) then
+--		local _ = log.info() and log.info(method, "No location found")
+--		return nil
+--	end
+--
+--	if vim.tbl_islist(result) then
+--		util.jump_to_location(result[1])
+--
+--		if #result > 1 then
+--			util.set_qflist(util.locations_to_items(result))
+--		end
+--	else
+--		util.jump_to_location(result)
+--	end
+--end
 
 icons = {
   lsp_signs = { Error = "", Warning = "", Hint = "", Information = "" },
