6b57c7bee27bb62c754fbf58b7fc81e5bcb8273b
Elia Cunial <elia.cunial@nhoa.energy>
2022-07-07 14:35:46 +0200

    Formatting and added new shortcut

--- lua/ide-settings/init.lua
+++ lua/ide-settings/init.lua
@@ -19,50 +19,49 @@
 --end
 
 icons = {
-  lsp_signs = { Error = "", Warning = "", Hint = "", Information = "" },
-  separators = { Left = '', Right = '' }
+	lsp_signs = { Error = "", Warning = "", Hint = "", Information = "" },
+	separators = { Left = "", Right = "" },
 }
 ide_settings = {
-  staline_theme = "normal",
-  indent = { rainbow = false },
-  language_servers = {
-    sumneko_lua = {
-      config = function(opts)
-        opts = vim.tbl_deep_extend("force", {
-          settings = {
-            Lua = {
-              runtime = { version = 'LuaJIT', path = vim.split(package.path, ';') },
-              diagnostics = { globals = { 'vim' } },
-              workspace = {
-                library = vim.api.nvim_get_runtime_file("", true),
-                checkThirdParty = false,
-                preloadFileSize = 10000
-              },
-              telemetry = { enable = false }
-            }
-          }
-
-        }, opts)
-        return opts
-      end
-    },
-    jsonls = {
-      config = function(opts)
-        opts = vim.tbl_deep_extend("force", {
-          settings = { json = { schemas = require('schemastore').json.schemas() } }
-        }, opts)
-        return opts
-      end
-    }
-    -- tsserver = {
-    --   config = function(opts)
-    --     opts = vim.tbl_deep_extend("force",
-    --       { handlers = { ["textDocument/definition"] = location_handler } },
-    --       opts)
-    --     return opts
-    --   end
-    -- }
-  }
+	staline_theme = "normal",
+	indent = { rainbow = false },
+	language_servers = {
+		sumneko_lua = {
+			config = function(opts)
+				opts = vim.tbl_deep_extend("force", {
+					settings = {
+						Lua = {
+							runtime = { version = "LuaJIT", path = vim.split(package.path, ";") },
+							diagnostics = { globals = { "vim" } },
+							workspace = {
+								library = vim.api.nvim_get_runtime_file("", true),
+								checkThirdParty = false,
+								preloadFileSize = 10000,
+							},
+							telemetry = { enable = false },
+						},
+					},
+				}, opts)
+				return opts
+			end,
+		},
+		jsonls = {
+			config = function(opts)
+				opts = vim.tbl_deep_extend("force", {
+					settings = { json = { schemas = require("schemastore").json.schemas() } },
+				}, opts)
+				return opts
+			end,
+		},
+		-- tsserver = {
+		--   config = function(opts)
+		--     opts = vim.tbl_deep_extend("force",
+		--       { handlers = { ["textDocument/definition"] = location_handler } },
+		--       opts)
+		--     return opts
+		--   end
+		-- }
+	},
 }
 
 UtilityFunctions = {
